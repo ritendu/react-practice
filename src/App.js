089fa2c5ef36260ae05594dd52723a4bc7062ae5
Ritendu Sen <ritendu.sen@weavers-web.com>
2022-08-15 00:22:23 +0530

    6th commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
-import items from './components/data'
-import Menu from './components/Menu';
-import Categories from './components/Categories'
-const allCategories = ['all',...new Set(items.map(item=>item.category))];
+import Loading from './components/Loading';
+import Jobs from './components/Jobs';
+const url = 'https://course-api.com/react-tabs-project'
+
+
 function App() {
-const [menuItems,setMenuItems]=useState(items);
-
-const filterItems = (category) => {
-  if (category === 'all') {
-    setMenuItems(items);
-    return;
+  const [loading,setLoading]= useState(true);
+  const [jobs,setJobs] = useState([])
+const fetchJobs = async()=>{
+  setLoading(true)
+  try {
+    const newJobs = await axios.get(url);  
+    setJobs(newJobs.data);
+    setLoading(false)
+  } catch (error) {
+    setLoading(true)
   }
-  const newItems = items.filter((item) => item.category === category);
-  setMenuItems(newItems);
-};
 
-
+}
+useEffect(()=>{
+fetchJobs()
+},[])
+if(loading){
+  return (
+    <Loading></Loading>
+  )
+}
 return (
   <>
-  <main>
-      <section className="menu section">
-        <div className="title">
-          <h2>our menu</h2>
-          <div className="underline"></div>
-        </div>
-        <Categories categories={allCategories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </section>
-    </main>
+<Jobs jobs={jobs}></Jobs>  
   </>
 )
 }
