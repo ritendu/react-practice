7b84949af00c145c5acb73bda9171872ad714926
Ritendu Sen <ritendu.sen@weavers-web.com>
2022-08-20 18:40:31 +0530

    form basics

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 
-import { useEffect, useState } from 'react';
+import File from './components/File';
 import './App.css';
-import Loading from './components/Loading';
-import Tours from './components/Tours';
-import axios from 'axios';
-const url = 'https://course-api.com/react-tours-project'
+
 
 function App() {
-const [loading,setLoading] = useState(true);
-const [tours,setTours] = useState([]);
-const fetchTours = async()=>{
-  setLoading(true);
-  try {
-    const tours = await axios(url);
-
-    setLoading(false);
-    setTours(tours);
-    
-  } catch (error) {
-    console.log()
-  }
-}
-useEffect(()=>{
-  fetchTours()
-},[])
-if(loading){
-  return (
-    <Loading></Loading>
-  )
-}
 
   return (
   <>
-<Tours tours={tours}></Tours>
+<File></File>
   </>
   
   );
