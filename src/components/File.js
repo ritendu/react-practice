cf8ae7754547bb0c88d9c5541477f38fd208d47b
Ritendu Sen <ritendu.sen@weavers-web.com>
2022-08-22 22:55:48 +0530

    forms

--- src/components/File.js
+++ src/components/File.js
@@ -1,34 +1,42 @@
 import { useState } from "react";
 
 const File = ()=>{
-    const [firstName,setFirstName] = useState('');
-    const [email,setEmail] = useState('');
+    // const [firstName,setFirstName] = useState('');
+    // const [email,setEmail] = useState('');
  const [people,setPeople] = useState([]);
-
+ const [person,setPerson] = useState({firstName:'',email:'',age:''});
+const handleChange = (e)=>{
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({...person,[name]:value})
+}
      const handleSubmit = (e)=>{
        e.preventDefault();
-const person = {id:new Date().getTime(),firstName,email};
-setPeople((people)=>{
-return [...people,person]
-})
+if(person.firstName && person.email && person.age){
+   const newPerson = {...person,id:new Date().getTime()};
+   setPeople([...people,newPerson]);
+   setPerson({firstName:'',email:'',age:''})
+}
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="firstName">FirstName</label>
-<input type="text" id="firstName" name="firstName" value={firstName} onChange={(e)=>setFirstName(e.target.value)}></input>
+<input type="text" id="firstName" name="firstName" value={person.firstName} onChange={handleChange}></input>
 <label htmlFor="email">Email</label>
-<input type="email" id="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+<input type="email" id="email" name="email" value={person.email} onChange={handleChange}></input>
+<input type="number" name="age" value={person.age} onChange={handleChange}></input>
 <button type="submit">submit</button>
             </form>
             {
                 people.map((person,index)=>{
-                    const {id,firstName,email} = person
+                    const {id,firstName,email,age} = person
                     return (
                         <div key={id}>
                             <h4>{firstName}</h4>
                            
                             <p>{email}</p>
+                            <p>{age}</p>
                         </div>
                     )
                 })
