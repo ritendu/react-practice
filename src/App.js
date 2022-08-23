f76220b551946647196bd6c4fc5736a1cd1ce40d
ritendu <ritendu.sen@weavers-web.com>
2022-08-23 19:38:03 +0530

    React-Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
-import File from './components/File';
-import './App.css';
 
+import './App.css';
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import { Home } from './components/Home';
+import {About} from './components/About'
+import { PageNotFound } from './components/PageNotFound';
 
 function App() {
 
   return (
-  <>
-<File></File>
-  </>
+ <BrowserRouter>
+ <Routes>
+  <Route path='/' element={<div><Home></Home></div>}></Route>
+  <Route path='about' element={<div><About></About></div>}></Route>
+  <Route path='*' element={<div><PageNotFound></PageNotFound></div>} ></Route>
+ </Routes>
+ </BrowserRouter>
   
   );
 }
