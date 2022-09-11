7f907d23b0a81c599e10217a03bfc537d2055d18
Ritendu Sen <ritendu.sen@weavers-web.com>
2022-09-11 22:53:40 +0530

    jj

--- src/components/ShortCircuitEvaluation.js
+++ src/components/ShortCircuitEvaluation.js
@@ -3,9 +3,15 @@ import { useState } from "react"
 const ShortCircuitEvaluation = ()=>{
     const [text,setText]= useState('peter');
     const [another,setAnother]= useState('Hellp')
+    const handleClick = (another)=>{
+setAnother((another)=>{
+    return !another;
+})
+    }
     return (
         <div>{text || 'Hello World'}
-        {another && 'Blob'}</div>
+        <button onClick={handleClick}>toggle</button>
+        {another? <p>Hello</p>:<p>world</p>}</div>
     )
 }
 
