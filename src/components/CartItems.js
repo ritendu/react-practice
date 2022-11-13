311287586c525d2e6c8c066ae6a1203679985963
Ritendu Sen <ritendu.sen@weavers-web.com>
2022-11-13 20:32:45 +0530

    redux

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,6 +1,24 @@
-const CartItems = ()=>{
+import { ChevronDown,ChevronUp } from "../icons"
+const CartItems = ({id,img,title,price,amount})=>{
+
     return (
-        <div> <p>CartItems</p></div>
+        <article className="cart-item">
+            <img src={img} alt={title} />
+            <div>
+                <h4>{title}</h4>
+                <h4 className="item-price">${price}</h4>
+                <button className="remove-btn">remove</button>
+            </div>
+            <div>
+                <button className="amount-btn">
+                    <ChevronUp></ChevronUp>
+                </button>
+                <p className="amount">{amount}</p>
+                <button className="amount-btn">
+                    <ChevronDown></ChevronDown>
+                </button>
+            </div>
+        </article>
     )
 }
 
