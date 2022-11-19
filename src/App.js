7861875fae72801b959a731e08c92b71848d8b45
Ritendu Sen <ritendu.sen@weavers-web.com>
2022-11-20 00:31:59 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
-import Loading from './components/Loading';
-import Jobs from './components/Jobs';
-const url = 'https://course-api.com/react-tabs-project'
+import data from './components/data'
 
 
 function App() {
-  const [loading,setLoading]= useState(true);
-  const [jobs,setJobs] = useState([])
-const fetchJobs = async()=>{
-  setLoading(true)
-  try {
-    const newJobs = await axios.get(url);  
-    setJobs(newJobs.data);
-    setLoading(false)
-  } catch (error) {
-    setLoading(true)
-  }
-
-}
-useEffect(()=>{
-fetchJobs()
-},[])
-if(loading){
-  return (
-    <Loading></Loading>
-  )
-}
-return (
-  <>
-<Jobs jobs={jobs}></Jobs>  
-  </>
-)
-}
+    const [people,setPeople] = useState(data)
+    console.log(people,"<-----------")
+   return (
+<section className='section'>
+<div className="title">
+<h2>
+    <span>/</span>reviews
+</h2>
+</div>
+<div className='section-center'>
+    {people.map((person,personIndex)=>{
+        const {id,image,name,title,quote} = person
+        return <article key={id}>
+<img src={image} alt={name} className="person-img"/>
+<h4>{name}</h4>
+<p className='title'>{title}</p>
+<p className='text'>{quote}</p>
+<FaQuoteRight />
+        </article>
+    })}
+    <button className='prev'><FiChevronLeft></FiChevronLeft></button>
+    <button className='next'><FiChevronRight></FiChevronRight></button>
+</div>
+</section>   
+   )
+ }
 
 export default App;
--- src/components/Loading.js
+++
@@ -1,7 +0,0 @@
-const Loading = ()=>{
-    return (
-        <div><h2>Loading...</h2></div>
-    )
-}
-
-export default Loading
