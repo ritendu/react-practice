88aed841ea317f06ba6ae6a9109ade2fce539070
Ritendu <ritendusen2@gmail.com>
2023-01-22 13:14:17 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import "./App.css";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import Submenu from "./components/Submenu";
+
 
 
 function App() {
+    const initialstate =0
+    const reducer = (state,action)=>{
+        if(action.type === 'increment'){
+            return state+1
+        }
+if(action.type === 'decrement'){
+    return state-1
+}
+    }
+    // const [count,setCount] = useState(0);
+    const [state,dispatch]=useReducer(reducer,initialstate)
 return (
 <>
-<Navbar/>
-<Sidebar/>
-<Hero/>
-<Submenu/>
+{state}
+<br/>
+<button onClick={()=>dispatch({type:'increment'})}>increment</button>
+<button onClick={()=>dispatch({type:'decrement'})}>decrement</button>
 </>
 )
 }
