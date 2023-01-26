8e2848534ad2ba6dabd40372d0c2104a03fd0b9e
Ritendu <ritendusen2@gmail.com>
2023-01-26 20:47:57 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import { useEffect, useReducer, useState } from "react";
 import "./App.css";
 
 
-
-function App() {
-    const initialstate =0
-    const reducer = (state,action)=>{
-        if(action.type === 'increment'){
-            return state+1
-        }
-if(action.type === 'decrement'){
-    return state-1
-}
+  
+const reducer = (state,action)=>{
+  console.log(state.count+1,action);
+  if(action.type === 'increment'){
+    const obj ={...state,count:state.count+1}
+    return obj;
+  }
+  
     }
-    // const [count,setCount] = useState(0);
-    const [state,dispatch]=useReducer(reducer,initialstate)
+function App() {
+   
+    const initialState = {
+        count:0
+    }
+
+const [state,dispatch]=useReducer(reducer,initialState);
+const handle = ()=>{
+    dispatch({type:'increment'})
+}
 return (
 <>
-{state}
-<br/>
-<button onClick={()=>dispatch({type:'increment'})}>increment</button>
-<button onClick={()=>dispatch({type:'decrement'})}>decrement</button>
+{state.count}
+<button onClick={handle}>increment</button>
 </>
 )
 }
---
+++ src/context/data.js
@@ -0,0 +1,9 @@
+// export const data =  [
+//     {
+// name:'abc',
+//     },
+//     {
+
+//     }
+
+// ]
