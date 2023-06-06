c4117879ca5bccbe630bdf05817273bde839a35d
ritendu <ritendu.sen@weavers-web.com>
2023-06-06 13:31:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { calculateTotals} from './features/cart/cartItems.slice';
+import { calculateTotals,getCartItems} from './features/cart/cartItems.slice';
 import { useEffect } from 'react';
 import Modal from './components/Modal';
 function App() {
   const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
-
+  console.log(cartItems,"isLoading")
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(()=>{
+dispatch(getCartItems())
+  },[])
 
   // useEffect(() => {
   //   dispatch(getCartItems('random'));
   // }, []);
 
-  // if (isLoading) {
-  //   return (
-  //     <div className='loading'>
-  //       <h1>Loading...</h1>
-  //     </div>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
--- src/features/cart/cartItems.slice.js
+++ src/features/cart/cartItems.slice.js
@@ -1,14 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 // import cartItems from "../cartItems"
 import cartItems from "../../cartItems";
 const initialState = {
-    cartItems:cartItems,
+    cartItems:[],
     amount:0,
     total:0,
     isLoading:true,
     isOpen:false
 
 }
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+    return fetch(url)
+      .then((resp) => resp.json())
+      .catch((error) => console.log(error));
+  });
 
 export const items = createSlice({
 name:'cart',
@@ -42,7 +50,22 @@ cartItem.amount = cartItem.amount -1
     openModal:(state)=>{
         state.isOpen = !state.isOpen
     },
-}
+    
+},
+extraReducers: {
+    [getCartItems.pending]: (state) => {
+        console.log('Hello')
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
 })
 
 export const {cleanCart,increase,decrease,remove,calculateTotals,openModal} = items.actions
