8d0d036b5b41975b40024df87b697cb27519a495
ritendu <ritendu.sen@weavers-web.com>
2024-01-11 12:37:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
-import axios from "axios"
-import { Container } from '@mui/material';
-import Header from './components/Header/Header';
-import Definations from './components/definations/Definations';
+import { useToggle } from './customHooks/Toggle';
+
 function App() {
-const [meanings,setMeanings] = useState([]);
-const [category, setCategory] = useState("en");
-const [word, setWord] = useState("");
-const [LightTheme, setLightTheme] = useState(false);
-  const fetchApi = async()=>{
-    try {
-      if(word!==""){
-        const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`)
-        console.log(data.data,"??????????")
-      setMeanings(data.data);
-  
-      }
-    } catch (error) {
-      
-    }
-    
-  }
-useEffect(()=>{
-  console.log(category,"??????")
-const fetch = setTimeout(()=>{
-  fetchApi()
-},2000)
-return ()=>clearTimeout(fetch)
-},[category,word])
-  return (
- <div className='App' style={{height:'100vh', backgroundColor:"#282c34", color:"white"}}>
- <Container maxWidth="md" style={{display:"flex",flexDirection:"column", height:"100vh"}}> 
-<Header category={category} setCategory={setCategory} word={word} setWord={setWord}/>
-{meanings && <Definations meanings={meanings} category={category} word={word}/>}
-  </Container>
-</div>
+  const [value,toggle] =useToggle(false)
+return (
+  <>
+  {value?'true' : 'false'}
+  <button onClick={toggle}>click me</button>
+  </>
 
+)
 
-  );
 }
 
 export default App;
